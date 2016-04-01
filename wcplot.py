114117123d31cf173ef7fdbbb4331180f7ac5eb2
Luke Zappia <lazappi@users.noreply.github.com>
2016-04-01 21:00:02 +1100

    Setup wcplot.py

--- wcplot.py
+++ wcplot.py
@@ -3,4 +3,38 @@
 Produce plots from prettytc log files
 """
 
+import argparse
+import pandas as pd
 
+def read_log(logpath):
+    """
+    Read prettytc log file
+    """
+
+    log = pd.read_table(logpath)
+
+    return log
+
+
+def get_args():
+    """
+    Get arguments from the command line using argparse
+    """
+
+    parser = argparse.ArgumentParser(prog="wcprint",
+                                     description="""Produce plots from prettytc
+                                                    log files""")
+    parser.add_argument("logfile", help="prettytc log file to plot")
+
+    arguments = parser.parse_args()
+
+    return arguments
+
+
+if __name__ == "__main__":
+
+    args = get_args()
+
+    data = read_log(args.logfile)
+
+    print(data)
